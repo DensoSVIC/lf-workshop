83bd7a5f7a5615a07851ce2b01cc2756fd6d6aab
Ravi Akella <ravi.c.akella@gmail.com>
2023-02-23 21:21:28 +0000

    Got Python examples to work

--- .scripts/setup.bash
+++ .scripts/setup.bash
@@ -28,6 +28,7 @@ fi
 ## Setup Python
 sudo apt install --assume-yes python3 python3-dev python3-pip
 python3 -m pip install --exists-action i setuptools requests
+pip3 install LinguaFrancaBase
 ## Setup Typescript
 npm install -g typescript
 ## Setup Rust
