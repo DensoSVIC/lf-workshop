5f2e220cd47361086cb78041927418fa413141db
Ravi Akella <ravi.akella@na.denso.com>
2023-02-27 20:46:46 -0800

    Mods to setup bash

--- .scripts/setup.bash
+++ .scripts/setup.bash
@@ -65,17 +65,20 @@ case "$RELEASE_BUILD" in
     *) 
         pip install requests
         ./.scripts/get-lf-executable $RELEASE_BUILD
-        mkdir lingua-franca
+        #mkdir lingua-franca-executable
         # While what we have here is tar.gz, lf release bot appear to have a bug and did not gunzip it.
         # Therefore `tar -xzf` will fail but `tar -xf` will work.
         # Here, we ignore the actual build name (the original name of the file and the original first directory). 
-        tar -xf lf.tar.gz -C lingua-franca --strip-components 1
-        rm lf.tar.gz
+        #tar -xf lf.tar.gz -C lingua-franca-executable --strip-components 1
+        #rm lf.tar.gz
 
         #Install RTI
         #git clone https://github.com/lf-lang/reactor-c.git && \ //This Fails
         #cd reactor-c/core/federated/RTI/ && \
         git clone https://github.com/lf-lang/lingua-franca.git --branch master --depth 1
+        ./.scripts/get-lf-executable $RELEASE_BUILD
+        tar -xf lf.tar.gz -C lingua-franca --strip-components 1
+        rm lf.tar.gz
         cd lingua-franca
         git submodule update --init --recursive
         cd org.lflang/src/lib/c/reactor-c/core/federated/RTI && \
